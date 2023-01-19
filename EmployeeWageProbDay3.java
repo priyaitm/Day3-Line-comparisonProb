3fb0251c1c55a2bef160949307314d82fc6caba2
priya <priyachauhan47365@gmail.com>
2023-01-19 15:24:52 -0600

    Added: Welcome Message!!

---
+++ EmployeeWageProbDay3.java
@@ -0,0 +1,7 @@
+package java_program;
+public class EmployeeWageProbDay3 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+    }
+}
