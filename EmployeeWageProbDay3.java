1d363f66a4b7dc032ef220287d4048d4f63a358d
priya <priyachauhan47365@gmail.com>
2023-01-19 15:48:02 -0600

    Added: Check Employee Presence

--- EmployeeWageProbDay3.java
+++ EmployeeWageProbDay3.java
@@ -2,6 +2,13 @@ package java_program;
 public class EmployeeWageProbDay3 {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
-
+        int Full_Time =1;
+        double empCheck = Math.floor(Math.random() * 10)%2 ;
+        if(empCheck == Full_Time) {
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
